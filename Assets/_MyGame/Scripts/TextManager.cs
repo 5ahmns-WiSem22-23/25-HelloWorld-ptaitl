0213130c3e6abca929f56299bb79758de3d4091c
Peter Taitl <peter.taitl@icloud.com>
2022-12-08 14:08:21 +0100

    added scripts

---
+++ Assets/_MyGame/Scripts/TextManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text text;
+
+    [SerializeField]
+    private InputField input;
+
+    public void LoadText()
+    {
+        text.text = input.text;
+    } 
+}
