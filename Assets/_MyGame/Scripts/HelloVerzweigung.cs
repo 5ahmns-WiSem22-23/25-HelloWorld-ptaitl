1f7d35049a563a1cc9c00adb74d9964fa4dd9159
Peter Taitl <peter.taitl@icloud.com>
2022-12-21 13:58:04 +0100

    added HelloVerzweigung

---
+++ Assets/_MyGame/Scripts/HelloVerzweigung.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class HelloVerzweigung : MonoBehaviour
+{
+    void Start()
+    {
+        int x = 1;
+
+        #region Variante "if"
+        if (x == 0)
+        {
+            Debug.Log("x ist 0");
+            return;
+        }
+
+        if(x == 1)
+        {
+            Debug.Log("x ist 1");
+            return;
+        }
+
+        if (x == 2)
+        {
+            Debug.Log("x ist 2");
+            return;
+        }
+        #endregion
+
+        #region Variante "else if"
+        if (x == 0)
+        {
+            Debug.Log("x ist 0");
+        }
+
+        else if (x == 1)
+        {
+            Debug.Log("x ist 1");
+        }
+
+        else if (x == 2)
+        {
+            Debug.Log("x ist 2");
+        }
+        #endregion
+
+        #region Variante "switch"
+        switch(x) {
+            case 0:
+                Debug.Log("x ist 0");
+                break;
+            case 1:
+                Debug.Log("x ist 1");
+                break;
+            case 2:
+                Debug.Log("x ist 2");
+                break;
+            default:
+                Debug.Log("x ist weder 0, noch 1 oder 2");
+                break;
+        }
+        #endregion
+    }
+}
